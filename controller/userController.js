fe7edff380efb00b9481801bd05a8446c0230030
amitsingh-1811 <humhiamithai@gmail.com>
2025-05-14 20:36:41 +0530

    deploying to render

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,10 @@ const User = require("../models/userModel");
 const bcrypt = require('bcrypt');
 const ErrorHandler = require("../utils/errorhandler");
 const sendToken = require("../utils/jwtToken");
+const jwt = require("jsonwebtoken");
+const nodemailer = require('nodemailer');
+const { clearRedisSet } = require("../utils/cache");
+
 exports.registerUser = async (req,res,next)=> {
 
     try{const {name,email,password} = req.body;
@@ -13,7 +17,11 @@ exports.registerUser = async (req,res,next)=> {
         password
     })
 
-    sendToken(user,201,res);}
+    sendToken(user,201,res);
+    console.log("user=> ",user, "token=> ",req.cookies.token);
+    const verificationLink = `http://localhost:4000/verify-email?token=${req.cookies.token}`
+    sentEmail(email, verificationLink)
+}
 
     catch(error){
         console.log("error is ",error);
@@ -42,8 +50,25 @@ exports.loginUser = async function(req,res,next) {
          if(!isPasswordMatched)
          return next(new ErrorHandler("invalid email or password",401));
 
+         console.log("user=> ",user)
         if(isPasswordMatched)
-        return sendToken(user,200,res);
+        {
+            console.log("user.isVerified=> ",user.isVerified)
+            if(user.isVerified==true){
+                clearRedisSet()
+                return sendToken(user,201,res);
+            }
+
+            else{
+                sendToken(user,200,res);
+                const verificationLink = `http://localhost:4000/verify-email?token=${req.cookies.token}`
+                sentEmail(email, verificationLink)
+                res.json({
+                    success:true,
+                    message: "verify your email."
+                })
+            }
+        }
     }
 
     catch(error){
@@ -67,4 +92,42 @@ exports.logoutUser = async function(req,res){
     catch(error){
         console.log("error is ",error);
     }
+}
+
+exports.verifyEmail = async (req, res) => {
+    try {
+        const user = await User.findById(req.id);
+        console.log("user=> ",user, "id=> ",req.id);
+        const result = await User.updateOne({ email: user?.email }, { $set: { isVerified: true } });
+        console.log("result=> ",result)
+        return res.send("Email verified successfully!");
+    } catch (error) {
+        return res.status(400).send("Invalid or expired token.");
+    }
+}
+
+const sentEmail = function (email, verificationLink){
+    console.log("email => ", email, "sendingEmail=> ",process.env.MAIL_USERNAME, "password=> ",process.env.MAIL_PASSWORD);
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.MAIL_USERNAME,
+            pass: process.env.MAIL_PASSWORD
+        }
+    })
+ 
+    let mailOptions = {
+        from: process.env.MAIL_USERNAME,
+        to: email,
+        subject: 'Email verification',
+        text: `Hello,\n\nPlease verify your email by clicking the link: ${verificationLink}\n\nThank you!`
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+            console.error("error=> ",error);
+        } else{
+            console.log("verification email sent: ", info.response)
+        }
+    });
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
-const {registerUser, loginUser, logoutUser} = require("../controller/userController.js");
+const {registerUser, loginUser, logoutUser, verifyEmail} = require("../controller/userController.js");
+const { isAuthenticatedUser, emailVerification } = require('../middleware/auth.js');
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
+router.route("/verify-email").get(emailVerification, verifyEmail);
 module.exports = router;
